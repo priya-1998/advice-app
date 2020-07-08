d2367b0710e4f7bcde5558bf0306627c81af41ca
Priya <lalpriya1620@gmail.com>
2020-07-08 13:06:23 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+
+//we need to import axios inorder to make a get request to certain api
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    state= { advice: '' };
+
+    //some random advice needs to be fetched at the beginning and for that we will use react lifecycle methods
+    componentDidMount(){
+        this.fetchAdvice();
+    }
+
+    //we don't need to write const here, because its a function inside a class hence would be treated as method
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+
+        //for response
+            .then((response) => {
+             //to destruct the advice from response
+                const { advice } = response.data.slip;
+
+                this.setState({ advice });
+            })
+
+            //for error
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{this.state.advice}</h1>
+                    <button className="button" onClick={this.fetchAdvice}>
+                        <span>ADVICE PLEASE!</span>
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
